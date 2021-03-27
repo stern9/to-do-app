e024902206b28d19e7a92f41e9829d9675789b10
stern9 <avram.stern@digitas.com>
2021-03-26 19:53:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Form from "./Form";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (i) =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : "",
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>reset</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const useInputValue = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  return {
+    value,
+    onChange: (e) => setValue(e.target.value),
+    resetValue: () => setValue(""),
+  };
+};
+
+const Form = ({ onSubmit }) => {
+  const { resetValue, ...text } = useInputValue("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(text.value);
+        resetValue();
+      }}
+    >
+      <input {...text} />
+    </form>
+  );
+};
+
+export default Form;
